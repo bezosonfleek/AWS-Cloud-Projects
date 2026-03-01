c9596de21340de956ce20e9f451ced7a635464b7
Geoffrey Sakora <162801736+bezosonfleek@users.noreply.github.com>
2026-03-02 01:57:28 +0300

    Create load-data.sh

---
+++ Load Data into DynamoDB/load-data.sh
@@ -0,0 +1,4 @@
+   aws dynamodb batch-write-item --request-items file://ContentCatalog.json
+   aws dynamodb batch-write-item --request-items file://Forum.json
+   aws dynamodb batch-write-item --request-items file://Post.json
+   aws dynamodb batch-write-item --request-items file://Comment.json
