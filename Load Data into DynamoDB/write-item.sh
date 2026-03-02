94275d83807d549ffa6465d6b0fae90db8f08b30
Geoffrey Sakora <162801736+bezosonfleek@users.noreply.github.com>
2026-03-02 22:36:02 +0300

    Delete Load Data into DynamoDB/write-item.sh

--- Load Data into DynamoDB/write-item.sh
+++
@@ -1,7 +0,0 @@
-aws dynamodb batch-write-item --request-items file://ContentCatalog.json
-
-aws dynamodb batch-write-item --request-items file://Forum.json
-
-aws dynamodb batch-write-item --request-items file://Post.json
-
-aws dynamodb batch-write-item --request-items file://Comment.json
