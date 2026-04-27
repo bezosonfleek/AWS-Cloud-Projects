f74af15d6fd9c43232d89fc3fe851cc4daf5d4d8
Geoffrey Sakora <162801736+bezosonfleek@users.noreply.github.com>
2026-04-27 09:11:15 +0300

    Update main.tf

--- IaC/VPC/main.tf
+++ IaC/VPC/main.tf
@@ -1,4 +1,25 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_vpc" "test-vpc" {
+  cidr_block = 10.0.0.0/24
+  tags = {
+    Name = test-vpc
+  }
+}
+
+resource "aws_subnet" "public-subnet-test" {
+  vpc.id = aws_vpc.test-vpc.id
+}
+
+resource "aws_subnet" "public-subnet-test" {
+  vpc.id = aws_vpc.test-vpc.id
+}
+
+resource "aws_security_group" "private-sg-test" {
+  ingress {
+  allow = 22
+  }
+
 #coming soon ...
