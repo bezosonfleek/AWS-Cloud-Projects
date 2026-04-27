d628b4ca4839622fe8a562ff1679a317ea732a6e
Sakora <geoffreysakora@gmail.com>
2026-04-27 18:02:55 +0300

    t

--- IaC/VPC/main.tf
+++ IaC/VPC/main.tf
@@ -1,4 +1,14 @@
 provider "aws" {
   region = "us-east-1"
 }
+resource "aws_vpc" "test-vpc" {
+  tags = {
+    Name = "test-vpc"
+  }
+  cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_subnet" "test-subnet" {
+  vpc_id = aws_vpc.test-vpc.id
+}
 #coming soon ...
