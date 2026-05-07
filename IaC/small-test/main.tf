e3a000dceb6c45c7f40e172b53a43708688742ea
Sakora <geoffreysakora@gmail.com>
2026-05-07 21:16:24 +0300

    p1 & p2

--- IaC/small-test/main.tf
+++ IaC/small-test/main.tf
@@ -10,3 +10,27 @@ resource "aws_instance" "test-instnce" {
         Name = "test-again"
     }
 }
+
+resource "aws_s3_bucket" "geoffrey-terraform-state" {
+    bucket = "geoffrey-terraform-state"
+}
+resource "aws_dynamodb_table" "terraform-lock-table" {
+    name           = "terraform-lock-table"
+    billing_mode   = "PAY_PER_REQUEST"
+    hash_key       = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "geoffrey-terraform-state"
+    key            = "network/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table" # This enables the locking!
+    encrypt        = true
+  }
+}
