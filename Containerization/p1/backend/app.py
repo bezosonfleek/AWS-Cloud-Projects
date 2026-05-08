2f69733d65e7ecb697c1a0010d338e44904a0d3f
Sakora <geoffreysakora@gmail.com>
2026-05-08 14:09:27 +0300

    p1

---
+++ Containerization/p1/backend/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_cors import CORS 
+import os 
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    return jsonify({"message": "Hello from the backend!"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
