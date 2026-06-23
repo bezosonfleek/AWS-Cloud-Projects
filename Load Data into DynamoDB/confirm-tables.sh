1e39abba086d2d7f6c7b8946613ec291167fbe22
Sakora <geoffreysakora@gmail.com>
2026-06-23 19:20:25 +0300

    chore: clear out duplicate legacy folders after directory refactor

--- Load Data into DynamoDB/confirm-tables.sh
+++
@@ -1,4 +0,0 @@
-aws dynamodb wait table-exists --table-name ContentCatalog
-aws dynamodb wait table-exists --table-name Forum
-aws dynamodb wait table-exists --table-name Post
-aws dynamodb wait table-exists --table-name Comment
--- Load Data into DynamoDB/delete-table.sh
+++
@@ -1,5 +0,0 @@
-aws dynamodb delete-table --table-name Comment
-aws dynamodb delete-table --table-name Forum
-aws dynamodb delete-table --table-name ContentCatalog
-aws dynamodb delete-table --table-name Post
-aws dynamodb delete-table --table-name NextWorkStudents
